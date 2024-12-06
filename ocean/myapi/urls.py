dec6a27f39400ce282f6fc8490e957a97672b221
Tiliphou <andoni.iparraguirre@gmail.com>
2024-12-06 02:18:36 +0100

    none

--- ocean/myapi/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ThreeJSView
-
-urlpatterns = [
-    path('threejs/', ThreeJSView.as_view(), name='threejs'),
-]
--- ocean/myapi/views.py
+++
@@ -1,9 +0,0 @@
-from django.views import View
-from django.shortcuts import render
-
-class ThreeJSView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'myapi/index.html', {'message': 'This is a GET request'})
-
-    def post(self, request, *args, **kwargs):
-        return render(request, 'myapi/index.html', {'message': 'This is a POST request'})
